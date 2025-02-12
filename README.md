eef08f04d06249f81fd6a957dca234ed2f0ed139
Nguyen Toan Thang <xamthang09@gmail.com>
2025-02-12 21:40:38 +0700

    UISP

---
+++ README.md
@@ -0,0 +1,1 @@
+# UISP-Project
---
+++ src/main/java/UISP/UispApplication.java
@@ -0,0 +1,13 @@
+package UISP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UispApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UispApplication.class, args);
+	}
+
+}
